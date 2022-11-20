0a42d262e692f159c0fa4e90b1ffdfe55cc19c3c
ArHiFranki <franki550@gmail.com>
2022-11-20 11:51:03 +0700

    Movement tweaks

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -18,7 +18,7 @@ namespace RPG.Movement
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
