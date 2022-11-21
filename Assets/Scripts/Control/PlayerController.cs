46ee9877b2212ce065f5eb2292557f97b6ad4e7b
ArHiFranki <franki550@gmail.com>
2022-11-21 14:56:45 +0700

    Refactoring PlayerController

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -27,8 +27,7 @@ namespace RPG.Control
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             foreach (RaycastHit hit in hits)
             {
-                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if (target == null) continue;
+                if (!hit.transform.TryGetComponent(out CombatTarget target)) continue;
 
                 if (Input.GetMouseButtonDown(0))
                 {
