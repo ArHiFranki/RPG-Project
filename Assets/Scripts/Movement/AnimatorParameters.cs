15aefcee8b8d9cd85ecfb9d3bd76a49f58a0d899
ArHiFranki <franki550@gmail.com>
2022-11-22 14:05:32 +0700

    Added attack animation trigger

--- Assets/Scripts/Movement/AnimatorParameters.cs
+++ Assets/Scripts/Movement/AnimatorParameters.cs
@@ -3,5 +3,6 @@ namespace RPG.Movement
     public static class AnimatorParameters
     {
         public const string ForwardSpeed = "forwardSpeed";
+        public const string Attack = "attack";
     }
 }
