c780fb5fcf66fecdef5ac6f63fb751cb3664f48a
ArHiFranki <franki550@gmail.com>
2022-11-22 14:43:07 +0700

    Added attack animation trigger feature

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -6,6 +6,7 @@ namespace RPG.Combat
 {
     [RequireComponent(typeof(Mover))]
     [RequireComponent(typeof(ActionScheduler))]
+    [RequireComponent(typeof(Animator))]
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] private float weaponRange = 2f;
@@ -13,11 +14,13 @@ namespace RPG.Combat
         private Transform target;
         private Mover myMover;
         private ActionScheduler myActionScheduler;
+        private Animator myAnimator;
 
         private void Awake()
         {
             myMover= GetComponent<Mover>();
             myActionScheduler = GetComponent<ActionScheduler>();
+            myAnimator = GetComponent<Animator>();
         }
 
         private void Update()
@@ -31,6 +34,7 @@ namespace RPG.Combat
             else
             {
                 myMover.Cancel();
+                AttackBehaviour();
             }
         }
 
@@ -45,9 +49,20 @@ namespace RPG.Combat
             target = null;
         }
 
+        private void AttackBehaviour()
+        {
+            myAnimator.SetTrigger(AnimatorParameters.Attack);
+        }
+
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        // Animation Event
+        private void Hit()
+        {
+            
         }
     }
 }
