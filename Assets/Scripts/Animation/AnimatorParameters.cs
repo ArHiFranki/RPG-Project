8a38d057edb7bdc15f85deb2a3c7f0ecacb72617
ArHiFranki <franki550@gmail.com>
2022-11-22 20:34:05 +0700

    Added additional enemies and animation triggers

--- Assets/Scripts/Animation/AnimatorParameters.cs
+++ Assets/Scripts/Animation/AnimatorParameters.cs
@@ -5,5 +5,6 @@ namespace RPG.Animation
         public const string ForwardSpeed = "forwardSpeed";
         public const string Attack = "attack";
         public const string Die = "die";
+        public const string StopAttack = "stopAttack";
     }
 }
